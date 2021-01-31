ccbce1ce635665a2d44f1e793ca4b0b166a51656
Krzysztof Przekwas <przekwaskrzysiek@gmail.com>
2021-01-31 21:34:33 +0100

    AUTh error handling

--- geo_api/security.py
+++ geo_api/security.py
@@ -11,3 +11,7 @@ def authenticate(username, password):
 def identity(payload):
     user_id = payload['identity']
     return UserModel.find_by_id(user_id)
+
+@jwt.error_handler
+def error_handler(e):
+    return "Authorization failed, try again later", 400
